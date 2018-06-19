a78f4638296dc3c39265c25fcaa207163459cd68
TristanEduProjet <TristanEduProjet@users.noreply.github.com>
2018-06-20 00:30:15 +0200

    Synchronize with mutex creation of database between instances

--- src/main/java/fr/esgi/ideal/DatabaseVerticle.java
+++ src/main/java/fr/esgi/ideal/DatabaseVerticle.java
@@ -13,6 +13,7 @@ import liquibase.resource.ClassLoaderResourceAccessor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Stream;
 
 @Slf4j
@@ -36,41 +37,9 @@ public class DatabaseVerticle extends AbstractVerticle {
                 .put("user", "ideal-api")
                 .put("password", "ideal-pwd")*/
                 .put("max_pool_size", 30));
-        log.debug("{}", conf);
         this.client = JDBCClient.createShared(this.vertx, conf);
-        this.client.getConnection(ar -> {
-            if(ar.succeeded()) {
-                try(final SQLConnection connection = ar.result()) {
-                    final Liquibase liquibase = new Liquibase(DB_CHANGELOG, new ClassLoaderResourceAccessor(), new JdbcConnection(connection.unwrap()));
-                    log.trace("liquibase's logger used : {}", liquibase.getLog().getClass().getName());
-                    //~ Get State
-                    log.debug("Use ChangeLog '{}'", liquibase.getChangeLogFile());
-                    Stream.of(liquibase.listLocks()).forEach(lock -> log.debug("lock in database : {} the {} by {}", lock.getId(), lock.getLockGranted(), lock.getLockedBy()));
-                    liquibase.listUnrunChangeSets(null, null)
-                            .forEach(cs -> log.info("Changelog unrun : {} by {} in file {}", cs.getId(), cs.getAuthor(), cs.getFilePath()));
-                    if(liquibase.isSafeToRunUpdate())
-                        log.info("Liquibase safe to update");
-                    else
-                        log.warn("Liquibase not safe to update");
-                    liquibase.update(new Contexts());
-                    //liquibase.validate(); //throws if not
-                    if(!liquibase.listUnrunChangeSets(null, null).isEmpty()) {
-                        log.warn("Failed to upgrade the database!");
-                        if(this.config().getBoolean("dropBeforeUpgrade", false)) {
-                            liquibase.dropAll();
-                            liquibase.update((Contexts) null);
-                            //no check because dropped
-                        } else
-                            throw new LiquibaseException("All changeSets have not been executed. Missing changeSets to liquibaseExecute !");
-                    } //else
-                    startFuture.complete();
-                } catch(final LiquibaseException e) {
-                    log.error("Error while checking/updating the database", e);
-                    startFuture.fail(e);
-                }
-            } else
-                startFuture.fail(ar.cause());
-        });
+        this.startLiquibase.setHandler(startFuture.completer());
+        this.initLiquibase();
         log.debug("Starting complete");
     }
 
@@ -79,5 +48,45 @@ public class DatabaseVerticle extends AbstractVerticle {
         log.debug("Stopping verticle");
         this.stop();
         this.client.close(stopFuture.completer());
+    }
+
+    private Future<Void> startLiquibase = Future.future();
+    private final AtomicBoolean toInit = new AtomicBoolean(true);
+    private synchronized void initLiquibase() {
+        if(toInit.compareAndSet(true, false)) {
+            this.client.getConnection(ar -> {
+                if(ar.succeeded()) {
+                    try(final SQLConnection connection = ar.result()) {
+                        final Liquibase liquibase = new Liquibase(DB_CHANGELOG, new ClassLoaderResourceAccessor(), new JdbcConnection(connection.unwrap()));
+                        log.trace("liquibase's logger used : {}", liquibase.getLog().getClass().getName());
+                        //~ Get State
+                        log.debug("Use ChangeLog '{}'", liquibase.getChangeLogFile());
+                        Stream.of(liquibase.listLocks()).forEach(lock -> log.debug("lock in database : {} the {} by {}", lock.getId(), lock.getLockGranted(), lock.getLockedBy()));
+                        liquibase.listUnrunChangeSets(null, null)
+                                .forEach(cs -> log.info("Changelog unrun : {} by {} in file {}", cs.getId(), cs.getAuthor(), cs.getFilePath()));
+                        if(liquibase.isSafeToRunUpdate())
+                            log.info("Liquibase safe to update");
+                        else
+                            log.warn("Liquibase not safe to update");
+                        liquibase.update(new Contexts());
+                        //liquibase.validate(); //throws if not
+                        if(!liquibase.listUnrunChangeSets(null, null).isEmpty()) {
+                            log.warn("Failed to upgrade the database!");
+                            if(this.config().getBoolean("dropBeforeUpgrade", false)) {
+                                liquibase.dropAll();
+                                liquibase.update((Contexts) null);
+                                //no check because dropped
+                            } else
+                                throw new LiquibaseException("All changeSets have not been executed. Missing changeSets to liquibaseExecute !");
+                        } //else
+                        this.startLiquibase.complete();
+                    } catch(final LiquibaseException e) {
+                        log.error("Error while checking/updating the database", e);
+                        this.startLiquibase.fail(e);
+                    }
+                } else
+                    this.startLiquibase.fail(ar.cause());
+            });
+        }
     }
 }
